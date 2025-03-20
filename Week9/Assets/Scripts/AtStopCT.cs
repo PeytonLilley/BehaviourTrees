2e925380494649ed624565f4104ae51af078f6dc
PeytonLilley <148352069+PeytonLilley@users.noreply.github.com>
2025-03-20 14:51:50 -0400

    Transfer functionality

--- Week9/Assets/Scripts/AtStopCT.cs
+++ Week9/Assets/Scripts/AtStopCT.cs
@@ -1,10 +1,14 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Conditions {
 
 	public class AtStopCT : ConditionTask {
+
+		public BBParameter<bool> Bus1AtStop;
+		public BBParameter<bool> Bus2AtStop;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -25,7 +29,15 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+			if (Bus1AtStop.value == true &&  Bus2AtStop.value == true)
+			{
+                return false;
+            }
+			else
+			{
+				return true;
+			}
+			
 		}
 	}
 }
--- Week9/Assets/Scripts/OffBusAT.cs
+++ Week9/Assets/Scripts/OffBusAT.cs
@@ -1,14 +1,19 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class OffBusAT : ActionTask {
 
+		public GameObject busStop;
+		float timer = 0;
+
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
 		protected override string OnInit() {
+			timer = 0;
 			return null;
 		}
 
@@ -16,6 +21,11 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
+			timer += Time.deltaTime;
+			if (timer > 3)
+			{
+				agent.transform.position = busStop.transform.position;
+			}
 			EndAction(true);
 		}
 
